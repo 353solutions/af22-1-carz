88c5cc05a93e7170284e783dfc9f364c42db6a2a
Miki Tebeka <miki@353solutions.com>
2022-03-09 15:56:29 +0200

    grpc definitions

--- cmd/marshal/main.go
+++ cmd/marshal/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/353solutions/carz/pb"
 )
@@ -28,4 +30,31 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("json:", len(jdata))
+
+	car := pb.Car{
+		Id:       "007",
+		DriverId: "Bond",
+		Location: &loc,
+		Time:     timestamppb.Now(),
+		Status:   pb.Status_FREE,
+	}
+	fmt.Println(car.String())
+	data, err = proto.Marshal(&car)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("car proto:", len(data))
+	jdata, err = json.Marshal(car)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("car json:", len(jdata))
+	fmt.Println(string(jdata))
+
+	jdata, err = protojson.Marshal(&car)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("car protojson:", len(jdata))
+	fmt.Println(string(jdata))
 }
